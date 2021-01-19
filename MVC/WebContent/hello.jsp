f6016ae31ff3f65618c395cf1adf6b4577b349e3
leticia <leticia@192.168.200.115>
2021-01-19 18:59:35 +0900

    MVC pattern2 , req.setAttribute , req.getRequestDispatcher <%= request.getAttribute{""} %> / ${}

---
+++ MVC/WebContent/hello.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+
+    <h1> JSP page </h1>
+    <h2> <%=request.getAttribute("m") %> </h2>
+    <h2>${m}</h2>
---
+++ MVC/src/mvc/helloServlet.java
@@ -0,0 +1,26 @@
+package mvc;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/servlet.page")
+public class helloServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		
+		String msg = "MVC pattern2 "; 
+		req.setAttribute("m", msg);
+		
+		 RequestDispatcher dispat = req.getRequestDispatcher("hello.jsp");
+		 dispat.forward(req,resp);
+	}
+
+}
+
