8eb8296a2611f0cdabb83b0715d655448157d7f3
leticia <leticia@192.168.200.115>
2021-01-22 15:09:47 +0900

    req.getRequestDispatcher/ <%=request.getAttribute(" ") %> / ${}

--- MVC/src/mvc/helloServlet.java
+++ MVC/src/mvc/helloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/servlet.page")
+@WebServlet(urlPatterns = "/servlet.do")
 public class helloServlet extends HttpServlet {
 	
 	@Override
