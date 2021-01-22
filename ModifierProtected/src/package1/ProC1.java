f4987b269e0e79eb26de9d8daee1233a1236fa72
leticia <leticia@192.168.200.115>
2021-01-22 15:03:04 +0900

    Access to protected available in the same package / by a subclass in another package

---
+++ ModifierProtected/src/package1/ProC1.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class ProC1 {
+	
+	protected String p =
+			"protected: within its own package and by a subclass in another package ";
+}
---
+++ ModifierProtected/src/package2/ProC2.java
@@ -0,0 +1,12 @@
+package package2;
+
+import package1.ProC1;
+
+public class ProC2 extends ProC1 {
+   public static void main(String [] args) {
+	   ProC2 obj = new ProC2();
+	   System.out.println(obj.p);
+		
+   }	
+
+	}
